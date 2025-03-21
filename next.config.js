0b8f8f85f9ea0f2c66c1703e4478c4570a54d7e6
tristan-rvk <tristan.schmale@reykjavik.is>
2025-03-21 19:21:18 +0100

    updated next config to fix pictures bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   images: {
     domains: ["new.adtravel.is"],
+    // TODO: fix this
+    // Temporary fix until a solution is found for the image optimization issue
+    unoptimized: true,
   },
 };
